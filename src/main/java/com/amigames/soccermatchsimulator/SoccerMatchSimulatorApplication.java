e55fe5a32720926665bfab8f8916cfc6ca0f8860
rafael <rafaelmchaves@gmail.com>
2019-02-03 13:08:43 -0200

    Inicio do projeto

---
+++ src/main/java/com/amigames/soccermatchsimulator/SoccerMatchSimulatorApplication.java
@@ -0,0 +1,14 @@
+package com.amigames.soccermatchsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerMatchSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerMatchSimulatorApplication.class, args);
+	}
+
+}
+
